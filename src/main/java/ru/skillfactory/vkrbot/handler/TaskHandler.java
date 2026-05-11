b58f9b471cc93eaef7d1f788c8dd278668bc89cd
Nikolay Kostin <nikolay-2708@yandex.ru>
2026-05-11 18:40:08 +0300

    add redis

--- src/main/java/ru/skillfactory/vkrbot/handler/TaskHandler.java
+++ src/main/java/ru/skillfactory/vkrbot/handler/TaskHandler.java
@@ -896,25 +896,4 @@ public class TaskHandler extends BaseHandler {
         if (size < 1024 * 1024) return String.format("%.1f KB", size / 1024.0);
         return String.format("%.1f MB", size / (1024.0 * 1024.0));
     }
-
-    // Внутренние классы состояний (хранят только ID)
-    private static class TaskCreationState implements Serializable {
-        private static final long serialVersionUID = 1L;
-        private int step = 0;
-        private String title;
-        private String description;
-        private List<String> completionCriteriaList;
-        private Long deadlineId;  // вместо Deadline
-
-        public int getStep() { return step; }
-        public void setStep(int step) { this.step = step; }
-        public String getTitle() { return title; }
-        public void setTitle(String title) { this.title = title; }
-        public String getDescription() { return description; }
-        public void setDescription(String description) { this.description = description; }
-        public List<String> getCompletionCriteriaList() { return completionCriteriaList; }
-        public void setCompletionCriteriaList(List<String> list) { this.completionCriteriaList = list; }
-        public Long getDeadlineId() { return deadlineId; }
-        public void setDeadlineId(Long deadlineId) { this.deadlineId = deadlineId; }
-    }
 }
--- src/main/java/ru/skillfactory/vkrbot/model/DeadlineCreationState.java
+++ src/main/java/ru/skillfactory/vkrbot/model/DeadlineCreationState.java
@@ -13,4 +13,15 @@ public class DeadlineCreationState implements Serializable {
     private String description;
     private LocalDateTime deadlineDate;
     private User student;
+
+    public int getStep() { return step; }
+    public void setStep(int step) { this.step = step; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public LocalDateTime getDeadlineDate() { return deadlineDate; }
+    public void setDeadlineDate(LocalDateTime deadlineDate) { this.deadlineDate = deadlineDate; }
+    public User getStudent() { return student; }
+    public void setStudent(User student) { this.student = student; }
 }
